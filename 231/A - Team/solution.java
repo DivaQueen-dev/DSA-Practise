b5947e7d878a7ddc57a158d82870be1029ba56d9
Asritha <189637525+DivaQueen-dev@users.noreply.github.com>
2026-07-27 20:37:22 +0530

    Add Team [A] from Codeforces

---
+++ 231/A - Team/solution.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+ 
+        int t = 1;
+        // t = fs.nextInt();
+        while (t-- > 0) {
+            int n=fs.nextInt();
+            int ans=0;
+            for(int i=0;i<n;i++)
+            {
+                int a=fs.nextInt();
+                int b=fs.nextInt();
+                int c=fs.nextInt();
+                if(a+b+c>=2)
+                {
+                    ans++;
+                }
+            }
+            out.println(ans);
+ 
+    }
+ 
+out.flush();
+}
+ 
+static class FastScanner {
+    private final BufferedReader br;
+    private StringTokenizer st;
+ 
+    FastScanner(InputStream in) {
+        br = new BufferedReader(new InputStreamReader(in));
+    }
+ 
+String next() throws IOException {
+    while (st == null || !st.hasMoreTokens()) {
+        String line = br.readLine();
+        if (line == null) return null;
+        st = new StringTokenizer(line);
+    }
+return st.nextToken();
+}
+ 
+int nextInt() throws IOException {
+    return Integer.parseInt(next());
+}
+ 
+long nextLong() throws IOException {
+    return Long.parseLong(next());
+}
+}
+}
